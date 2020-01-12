bcda6851224fce80d7edcd80706be6ac74d3fead
LeonShimazaki <48808986+LeonShimazaki@users.noreply.github.com>
2020-01-13 02:13:27 +1300

    Update test.kt

--- test.kt
+++ test.kt
@@ -1,12 +1,25 @@
+package demo 
 
-/**Leon Shimazaki Coding practice */
+import java.util.Random
 
-package demo
 
-fun main(args : Arrayy<String>){
- println("Hello, world!")
- 
- fun add(num1: Int, num2: Int) = num1 + num2
- println("5 + 4 = ${add(5,4)}")
- 
+fun main(args : Array<String>){
+    println("Hello, world!")
+    
+    fun add(num1: Int, num2: Int) = num1 + num2
+    println("5 + 4 = ${add(5,4)}")
+
+    val items = listOf("apple", "banana", "kiwifruit", "orange")
+    for (item in items) {
+        println(item)
+    }
+    val rand = Random()
+    val magicnumber = rand.nextInt(50) + 1
+
+    var guess = 0
+
+    while(magicnumber != guess){
+        guess += 1
+    }
+    println("Magic number was $guess")
 }
